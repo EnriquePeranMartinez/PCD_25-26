7a7310b5f53ba5d24335e077b6f1e67a75141c20
El-Macabeo <albaviser@gmail.com>
2026-04-29 21:04:39 +0200

    Actualización de  Aficionado
    Se ha añadido la parte más básica para la ejecución del hilo

--- PCD/src/boletin/ejercicio4/Aficionado.java
+++ PCD/src/boletin/ejercicio4/Aficionado.java
@@ -1,1 +1,15 @@
 package boletin.ejercicio4;
+
+//Recuerda meter en el proyecto si no está, la biblioteca que se descarga de clase messagepassing
+import messagepassing.MailBox;
+import messagepassing.CommunicationScheme;
+
+public class Aficionado implements Runnable {
+  
+
+
+
+  public void run(){
+    
+  }
+}
