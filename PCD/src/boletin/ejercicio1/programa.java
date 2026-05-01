31cf7af980a2209ef60eadf4a02b26be8c6ff78f
El-Macabeo <albaviser@gmail.com>
2026-05-01 21:23:48 +0200

    Main del ejercicio1

---
+++ PCD/src/boletin/ejercicio1/programa.java
@@ -0,0 +1,14 @@
+package boletin.ejercicio1;
+
+//import java.util.concurrent.locks.ReentrantLock;
+
+public class Programa{
+
+  //private ReentrantLock pantalla = new ReentrantLock();
+  Matriz a = new Matriz();
+  Matriz b = new Matriz();
+  Matric c = inicializar0();
+  public static void main(String[] args){
+      
+  }
+}
